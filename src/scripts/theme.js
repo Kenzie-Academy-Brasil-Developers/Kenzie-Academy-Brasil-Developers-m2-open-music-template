6f3053f5dbfd8de18fbe960b1be4fd9310c49062
JulyannaGama <julyannagama6@gmail.com>
2024-01-10 09:39:54 -0300

    Tarefa 1 concluída

--- src/scripts/theme.js
+++ src/scripts/theme.js
@@ -1,1 +1,21 @@
 /* Desenvolva sua lógica aqui ... */
+function darkMode() {
+  const button = document.querySelector(".header__btn");
+  const root = document.querySelector("html");
+  const storage = localStorage.getItem("darkmode");
+  if (storage === "true") {
+    button.classList.toggle("header__btn--dark-mode");
+    root.classList.toggle("dark-mode");
+  }
+  button.addEventListener("click", () => {
+    button.classList.toggle("header__btn--dark-mode");
+    root.classList.toggle("dark-mode");
+    if (root.classList.contains("dark-mode")) {
+      localStorage.setItem("darkmode", "true");
+    } else {
+      localStorage.setItem("darkmode", "false");
+    }
+  });
+}
+
+darkMode();
